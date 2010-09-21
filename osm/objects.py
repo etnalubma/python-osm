81daaa0a9d7d2f4c82d8fb6abaf8aa0f8cbdac71
etnalubma <francisco.herrero@gmail.com>
2010-09-20 21:58:24 -0300

    missing something on commit before

--- osm/objects.py
+++ osm/objects.py
@@ -1,13 +1,16 @@
 
 class Node(object):
     """
-    This module creates a node:
-     >>> from osm.objects import Node
-     >>> n = Node(12, lon=1.5, lat=1.7, tags= {'name':'point1'}) 
-     >>> repr(n)
-     "Node(id=12, lon=1.5, lat=1.7, tags={'name': 'point1'})"
+    Node Object
+    -----------
+    
+    >>> from osm.objects import Node
+    >>> n = Node(12, lon=1.5, lat=1.7, tags= {'name':'point1'}) 
+    >>> repr(n)
+    "Node(id=12, lon=1.5, lat=1.7, tags={'name': 'point1'})"
     
     """
+
     def __init__(self, id=None, lon=None, lat=None, tags={}):
         self.id = id
         self.lon, self.lat = lon, lat
@@ -17,6 +20,17 @@ class Node(object):
         return "Node(id=%r, lon=%r, lat=%r, tags=%r)" % (self.id, self.lon, self.lat, self.tags)
 
 class Way(object):
+    """
+    Way Object
+    -----------
+    
+    >>> from osm.objects import Way
+    >>> w = Way(15, tags={'name':'way1'})
+    >>> repr(w)
+    "Way(id=15, nodes=[], tags={'name': 'way1'})"
+    
+    """
+
     def __init__(self, id, nodes=[], tags={}):
         self.id = id
         self.nodes = nodes
@@ -26,8 +40,18 @@ class Way(object):
         return "Way(id=%r, nodes=%r, tags=%r)" % (self.id, self.nodes, self.tags)
 
 class NodePlaceHolder(object):
+    """
+    NodePlaceHolder Object
+    ----------------------
+    
+    >>> from osm.objects import NodePlaceHolder
+    >>> n = NodePlaceHolder(id=2)
+    >>> repr(n)
+    'NodePlaceHolder(id=2)'
+    """
     def __init__(self, id):
         self.id = id
 
     def __repr__(self):
         return "NodePlaceHolder(id=%r)" % (self.id)
+
--- osm/parser.py
+++ osm/parser.py
@@ -1,19 +1,18 @@
 import xml.sax
 from objects import Node, Way, NodePlaceHolder
+from copy import deepcopy
 
-class OSMXMLFile(object):
+class OSMFile(object):
     def __init__(self, filename):
         self.filename = filename
-
         self.nodes = {}
         self.ways = {}
         self.__parse()
-        print repr(self.ways)
 
     def __parse(self):
         """Parse the given XML file"""
         parser = xml.sax.make_parser()
-        parser.setContentHandler(OSMXMLFileParser(self))
+        parser.setContentHandler(OSMFileParser(self))
         parser.parse(self.filename)
 
         # now fix up all the refereneces
@@ -25,32 +24,34 @@ class OSMXMLFile(object):
         self.ways = self.ways.values()
 
 
-class OSMXMLFileParser(xml.sax.ContentHandler):
+class OSMFileParser(xml.sax.ContentHandler):
     def __init__(self, containing_obj):
         self.containing_obj = containing_obj
         self.curr_node = None
         self.curr_way = None
-
+        self.curr_tags = {}
     def startElement(self, name, attrs):
         if name == 'node':
             self.curr_node = Node(id=attrs['id'], lon=attrs['lon'], lat=attrs['lat'])
         elif name == 'way':
             self.curr_way = Way(id=attrs['id'])
         elif name == 'tag':
-            if self.curr_node:
-                self.curr_node.tags[attrs['k']] = attrs['v']
-            elif self.curr_way:
-                self.curr_way.tags[attrs['k']] = attrs['v']
+            #import pdb;pdb.set_trace()
+            self.curr_tags[attrs['k']] = attrs['v'] 
         elif name == "nd":
             assert self.curr_node is None, "curr_node (%r) is non-none" % (self.curr_node)
             assert self.curr_way is not None, "curr_way is None"
             self.curr_way.nodes.append(NodePlaceHolder(id=attrs['ref']))
 
-
     def endElement(self, name):
         if name == "node":
-            self.containing_obj.nodes[self.curr_node.id] = self.curr_node
+            self.curr_node.tags = deepcopy(self.curr_tags)
+            self.containing_obj.nodes[self.curr_node.id] = self.curr_node 
             self.curr_node = None
+            self.curr_tags = {}
         elif name == "way":
+            self.curr_way.tags = deepcopy(self.curr_tags)
             self.containing_obj.ways[self.curr_way.id] = self.curr_way
             self.curr_way = None
+            self.curr_tags = {}
+        
